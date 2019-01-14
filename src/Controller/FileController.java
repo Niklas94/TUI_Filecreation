3155f7b432f37a0fa147e891c89128bee1744b42
NiklasThielemann <t.niklas@gmail.com>
2019-01-14 11:15:05 +0100

    Changes to filecontroller, tui and mainmenu

--- src/Controller/FileController.java
+++ src/Controller/FileController.java
@@ -1,19 +1,48 @@
 package Controller;
 
+import Interfaces.iFileController;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 
-public class FileController {
-	
-	public FileController() {
+public class FileController implements iFileController {
 
-		
-		
+	BufferedWriter bw;
+
+    @Override
+    public boolean createFile(String fileName) throws IOException {
+        File newFile = new File("C:\\Users\\NiklasThielemann\\Desktop\\" + fileName);
+        if (!newFile.exists()) {
+            newFile.createNewFile();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean chooseFileToLoad(String fileName) {
+        File newFile = new File("C:\\Users\\NiklasThielemann\\Desktop\\" + fileName);
+        if (newFile.exists()) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean writeToFile(String fileName, String input) {
+        File newFile = new File("C:\\Users\\NiklasThielemann\\Desktop\\" + fileName);
+        if (newFile.exists()) {
+            return true;
+        }
+        return false;
+    }
+
+    public enum options {
+		CREATE, LOAD, WRITE, EXIT, UNKNOWN
 	}
 
-	public static void work(String select) {
-		// TODO Auto-generated method stub
-		
-	}
 	
 	
 	
